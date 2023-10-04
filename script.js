3fe231a928bd86f93bf5f7d4748a601aca4b2f37
ryanthayes <127283656+ryanthayes@users.noreply.github.com>
2023-10-04 16:48:41 -0700

    Add mouseover "paint" effect

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('.container');
 const flexGrid = document.querySelector('.flex-grid');
+const gridItem = document.querySelector('.grid-item');
 
 // Create a 16x16 grid
 function makeGrid(rows, cols) {
@@ -7,7 +8,12 @@ function makeGrid(rows, cols) {
         const cell = document.createElement('div');
         cell.innerText = (i + 1);
         flexGrid.appendChild(cell).className = "grid-item";
+        // Paint cells on mouseover
+        cell.addEventListener('mouseover', () => {
+            cell.style.background = 'pink';
+        })
     }
 }
 
 makeGrid(16, 16);
+
