7705ad08ae0029f8328fc32ceb49ef255d4afe8f
ryanthayes <127283656+ryanthayes@users.noreply.github.com>
2023-10-04 15:34:08 -0700

    Create grid with flexbox

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const container = document.querySelector('.container');
+const flexGrid = document.querySelector('.flex-grid');
 
-// Create a 16x16 grid of square divs
+// Create a 16x16 grid
 function makeGrid(rows, cols) {
     for(i = 0; i < (rows * cols); i++) {
         const cell = document.createElement('div');
         cell.innerText = (i + 1);
-        container.appendChild(cell);
+        flexGrid.appendChild(cell).className = "grid-item";
     }
 }
 
