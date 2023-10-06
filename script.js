7c40fb119bacbdf2a33f279b682f20ef122d170b
ryanthayes <127283656+ryanthayes@users.noreply.github.com>
2023-10-06 11:04:03 -0700

    Added input for user to determine size of canvas

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const container = document.querySelector('.container');
 const flexGrid = document.querySelector('.flex-grid');
-const gridItem = document.querySelector('.grid-item');
+const gridSize = document.querySelector('.grid-size');
 
 // Create a 16x16 grid
 function makeGrid(rows, cols) {
+    let cell = flexGrid.querySelectorAll('div');
+    cell.forEach((div) => div.remove());
     for(i = 0; i < (rows * cols); i++) {
-        const cell = document.createElement('div');
+        cell = document.createElement('div');
         cell.innerText = (i + 1);
         flexGrid.appendChild(cell).className = "grid-item";
         // Paint cells on mouseover
@@ -16,4 +18,10 @@ function makeGrid(rows, cols) {
 }
 
 makeGrid(16, 16);
+
+function changeCanvasSize(input) {
+    let rows = input;
+    let cols = input;
+    makeGrid(rows, cols);
+}
 
