223a3ff60e7c0c67fc75d864ace6ebe39860526d
ryanthayes <127283656+ryanthayes@users.noreply.github.com>
2023-10-07 20:11:13 -0700

    Create button (color, reset) functionality and clear canvas

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-// Global variables
+// Global variables 
+const BLACK = 'var(--clr-neutral-900)';
+const GRAY = 'var(--clr-neutral-400)';
 
-const sizeOfGrid = 4;
+let color = BLACK;
+let sizeOfGrid = 16;
+let click = false;
 
-const eraserButton = document.getElementById('btn.erase');
-const resetButton = document.getElementById('btn.reset');
-
+const slider = document.getElementById('canvas-size-input');
+const eraserButton = document.getElementById('btn-eraser');
+const clearButton = document.getElementById('btn-clear');
+const gridSizeButton = document.getElementById('canvas-size-button');
 const canvas = document.getElementById('canvas');
-const setCanvasSize = document.getElementById('set-canvas-size');
 
 // Create a square grid
 function createGrid(numOfGrids) {
@@ -15,21 +19,52 @@ function createGrid(numOfGrids) {
         row.classList.add('row');
 
         for (let j = 0; j < numOfGrids; j++) {
+            const widthAndHeight = 500 / sizeOfGrid; // Divide size of canvas (set in CSS) by number of pixels in grid
             const cell = document.createElement('div');
             cell.classList.add('cell');
-            cell.style.width = '100%';
-            let height = 500 / parseInt(sizeOfGrid);
-            cell.style.height = `${height}px`;
-            cell.innerText = (j + 1);
+            cell.style.width = `${widthAndHeight}px`;
+            cell.style.height = `${widthAndHeight}px`;
+            cell.style.backgroundColor = GRAY;
             row.appendChild(cell);
 
             // Change background color of cell when drawing
-            cell.addEventListener('mouseenter', () => {
-            cell.style.backgroundColor = 'black';
-            });
+            cell.addEventListener('mouseenter', colorCell);
         }
         canvas.appendChild(row);
     }
 }
 
+// On Page Load - default size
 createGrid(sizeOfGrid);
+
+// UI controls
+
+// Range Bar and Display:
+
+
+// Change color of paint
+function colorCell() {
+    if (click) {
+        if((color === 'RANDOM')) {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        } else {
+            this.style.backgroundColor = color;
+        }
+    }
+}
+
+function changeColor(colorChoice) {
+    color = colorChoice;
+}
+
+// Clear grid
+function clear() {
+    let cells = canvas.querySelectorAll('div');
+    cells.forEach((div) => div.style.backgroundColor = GRAY);
+}
+
+// Event listeners
+clearButton.addEventListener('click', clear);
+document.querySelector("body").addEventListener('click', () => {
+    click = !click;
+})
